ed63e81ab1d78440c8555ec47aaf1ede7e030fdc
DESKTOP-8KMHP2P\JYS <noriks@nate.com>
2024-04-17 17:56:49 +0900

    firebase setting

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbvLVmJkS9S5yJ4uSawtSqjxN30jlp8Dc",
+  authDomain: "nwitter-reloaded-779f5.firebaseapp.com",
+  projectId: "nwitter-reloaded-779f5",
+  storageBucket: "nwitter-reloaded-779f5.appspot.com",
+  messagingSenderId: "922156253358",
+  appId: "1:922156253358:web:d76ee859888091feda2d3f",
+};
+
+const app = initializeApp(firebaseConfig);

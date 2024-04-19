b1354e32f56a7edca0b52533a4875383ef51ce2c
DESKTOP-8KMHP2P\JYS <noriks@nate.com>
2024-04-19 12:35:51 +0900

    Fetching Timeline(Search Data)

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -81,7 +81,7 @@ export default function PostTweetForm() {
       setLoading(true);
       const doc = await addDoc(collection(db, "tweets"), {
         tweet,
-        createAt: Date.now(),
+        createdAt: Date.now(),
         username: user.displayName || "Anonymouse",
         userId: user.uid,
       });
